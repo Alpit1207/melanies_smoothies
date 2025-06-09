6ebdc7e8e7e34294b749ed603adce0359b3af6e4
Alpit1207 <133636118+Alpit1207@users.noreply.github.com>
2025-06-09 16:28:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,13 @@ cnx = st.connection(
     schema="PUBLIC"
 )
 session = cnx.session()
-my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON')
+                                                                      )
 # st.dataframe(data=my_dataframe, use_container_width=True)
+#  convert the snowpark dataframe to a pandas dataframe so we use the loc function
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()   
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -37,10 +42,16 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = smoothiefroot_response.json()
         st.dataframe(data=sf_df, use_container_width=True)
+        # st.stop()
+
 
 
     my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
@@ -48,4 +59,4 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+        st.success('Your Smoothie is ordered!', icon="✅")  
